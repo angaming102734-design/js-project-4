8082b1d9b449210acc1e0e9211ef9ca4bb1177ef
ather ali <a.ngaming102734@gmail.com>
2025-12-02 21:57:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var sum=prompt('13+9*9');
+
+if (sum === "94") {
+    alert("Correct!");
+  }
+  else {
+    alert("Wrong answer please retry");
+  }
+  var shoo =('Prise of item 1 500'+'<br>');
+  var item =('Quantity of item 8'+'<br>');
+  var prise =('8'+'item prise 4000'+'<br>');
+  var petrol =('Petrol '+'700'+'RS'+'<br>')
+  var total =('4000+700'+'<br>')
+  document.write(shoo+item+prise+petrol+total);  
+    var mUu =('Multipication tabel 2')
+    document.write(mUu + '<br>')
+
+    var teBal = '2*1=2'
+    document.write(teBal+'<br>')
+
+     var aditio ='178 + 97';
+var sume ='755 - 99';
+document.write(aditio+sume)
+
+   
+ var adi=('98+22');
+ document.write=prompt(adi);
+ if (adi === "120"){
+  alert('you are Correct')
+ }
+
+var mi='100*2';
+document.write=prompt(mi);
+if (mi === "200"); {
+  alert("Write Good Answer");
+}
